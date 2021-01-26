3f1b0c1dc727933b58b86389d8d5eedf18acf2c6
gotouu21 <gotouu21@gmail.com>
2021-01-26 15:08:59 +0200

    Selector + form template

---
+++ front/src/components/04Admin/AdminCreateUser.jsx
@@ -0,0 +1,74 @@
+import React, { Component } from 'react'
+
+import http from '../10Services/httpService';
+import apiEndpoint from '../10Services/endpoint';
+
+import NavBar from '../00Navigation/NavBar';
+
+export default class AdminCreateUser extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            role: "admin"
+        }
+    }
+    roleDropdownOnChange = e => {
+        console.log(e.target.value);
+        this.setState({
+            role: e.target.value
+        })
+    }
+    const 
+    render() {
+        return (
+            <div>
+                <h1>Naujo vartotojo sukūrimas</h1>
+                <label for="role-selector">Vartotojo rolė:</label>
+                <select name="role-selector" className="selectpicker" onChange={this.roleDropdownOnChange}>
+                    <option value="admin">Administratorius</option>
+                    <option value="worker">Ugdymo istaigos darbuotojas</option>
+                    <option value="user">Vartotojas</option>
+                </select>
+                <form>
+                    <div className="form-group">
+                        <label for="txtName">Vardas:</label>
+                        <input type="text" className="form-control" id="txtName"></input>
+                    </div>
+                    <div className="form-group">
+                        <label for="txtLastname">Pavardė:</label>
+                        <input type="text" className="form-control" id="txtLastname"></input>
+                    </div>
+                    <div className="form-group">
+                        <label for="txt">Gimimo data:</label>
+                        <input type="date" className="form-control" id="txtBirthdate"></input>
+                    </div>
+                    <div className="form-group">
+                        <label for="txtIdentificationCode">Asmens kodas:</label>
+                        <input type="number" className="form-control" id="txtIdentificationCode"></input>
+                    </div>
+                    <div className="form-group">
+                        <label for="txtAddress">Deklaruota gyv. vieta:</label>
+                        <input type="text" className="form-control" id="txtAddress"></input>
+                    </div>
+                    <div className="form-group">
+                        <label for="txtTelNo">Telefonas:</label>
+                        <input type="number" className="form-control" id="txtTelNo"></input>
+                    </div>
+                    <div className="form-group">
+                        <label for="txtEmail">El. paštas:</label>
+                        <input type="text" className="form-control" id="txtEmail"></input>
+                    </div>
+                    <div className="form-group">
+                        <label for="txtUsername">Vartotojo vardas:</label>
+                        <input type="text" className="form-control" id="txtUsername"></input>
+                    </div>
+                    <div className="form-group">
+                        <label for="txtPassword">Slaptažodis:</label>
+                        <input type="text" className="form-control" id="txtPassword"></input>
+                    </div>
+                </form>
+
+            </div>
+        )
+    }
+}

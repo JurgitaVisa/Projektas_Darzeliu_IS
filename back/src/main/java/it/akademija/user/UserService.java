67eca4e3c4b4450e96d38b7b1e8db83a691d7cb7
JurgitaVisa <jurgita.visakaviciute@gmail.com>
2021-01-27 15:30:26 +0200

    userservice ištrina firstuser, kai įvedami nauji
    pom.xml patikslintas, kad nemestų perspėjimų console

--- back/src/main/java/it/akademija/user/UserService.java
+++ back/src/main/java/it/akademija/user/UserService.java
@@ -18,48 +18,77 @@ public class UserService implements UserDetailsService {
 
 	@Autowired
 	private UserDAO userDao;
+	
+	private PasswordEncoder encoder = new BCryptPasswordEncoder();
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		User user = findByUsername(username);
-		
-		if (user == null) {
-			throw new UsernameNotFoundException(username+" not found.");
-		} else {
-			return new org.springframework.security.core.userdetails.User(
-		user.getUsername(), user.getPassword(),
-		AuthorityUtils.createAuthorityList(
-		new String[] { "ROLE_" + user.getRole().toString() }) );
-		}	
-		
-	}	
 
+		if (user == null) {
+			throw new UsernameNotFoundException(username + " not found.");
+		} else {
+			return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+					AuthorityUtils.createAuthorityList(new String[] { "ROLE_" + user.getRole().toString() }));
+		}
+
+	}
+
+	/**
+	 * Create new user with specified parameters. Deletes FirstUser "admin" which
+	 * was initialized at start up if there are other users with ADMIN authorization
+	 * in the user repository
+	 * 
+	 * @param userData
+	 */
 	@Transactional
 	public void createUser(UserDTO userData) {
-		User newUser = new User(); 
-		PasswordEncoder encoder = new BCryptPasswordEncoder();
-//		PasswordEncoder encoder =
-//				PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		User newUser = new User();
 		
 		newUser.setUsername(userData.getUsername());
 		newUser.setName(userData.getName());
 		newUser.setSurname(userData.getSurname());
 		newUser.setPassword(encoder.encode(userData.getPassword()));
 		newUser.setRole(Role.valueOf(userData.getRole()));
-		
+
 		userDao.save(newUser);
+
+		if (userData.getRole().equals("ADMIN") && userDao.findByRole(Role.ADMIN).size() > 1 && userDao.findByUsername("admin")!=null) {
+			userDao.deleteByUsername("admin");
+		}
+
 	}
 
+	/**
+	 * 
+	 * Delete user with a specified username. In case there are no other users with
+	 * ADMIN authorization in the user repository, creates a temporary user with
+	 * username "admin" and password "laikinas"
+	 * 
+	 * @param username
+	 */
 	@Transactional
 	public void deleteUser(String username) {
+		
+		if (findByUsername(username).getRole().equals(Role.ADMIN) && userDao.findByRole(Role.ADMIN).size() == 1) {
+			userDao.save(new User("admin", "admin", "admin", encoder.encode("laikinas"), Role.ADMIN));
+		}
+		
 		userDao.deleteByUsername(username);
 	}
-	
+
+	/**
+	 * 
+	 * Finds user with a specified username
+	 * 
+	 * @param username
+	 * @return
+	 */
 	@Transactional(readOnly = true)
 	public User findByUsername(String username) {
-		
+
 		return userDao.findByUsername(username);
-		
+
 	}
 
 	public UserDAO getUserDao() {
@@ -70,7 +99,4 @@ public class UserService implements UserDetailsService {
 		this.userDao = userDao;
 	}
 
-	
-
-	
 }

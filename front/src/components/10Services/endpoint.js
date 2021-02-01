dca86effa651eafc6a258f64485cb0c63ec30c7e
Paulius <paujak@gmail.com>
2021-02-01 16:59:57 +0200

    apiEndpoint -> localhost

--- front/src/components/10Services/endpoint.js
+++ front/src/components/10Services/endpoint.js
@@ -3,7 +3,7 @@
 
 //dirbant lokaliai per yarn start paleidus react app per localhost:3000
 //pakeisti localhost:8080 į vartus ant kurių paleidžiat aplikaciją:
- //const apiEndpoint = "http://localhost:8080";
+ const apiEndpoint = "http://localhost:8080";
 
  
  //Jurgitos endpoint :)

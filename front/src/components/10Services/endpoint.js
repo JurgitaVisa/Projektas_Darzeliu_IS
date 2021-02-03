6c85833c90067b47e842ba3a66c7d00e5c4a00df
Paulius <paujak@gmail.com>
2021-02-03 12:10:58 +0200

    endpoint

--- front/src/components/10Services/endpoint.js
+++ front/src/components/10Services/endpoint.js
@@ -1,9 +1,5 @@
 //prieš darant yarn build užkomentuoti localhost ir atkomentuoti šitą endpoint:
-<<<<<<< HEAD
-// const apiEndpoint = process.env.PUBLIC_URL;
-=======
 //const apiEndpoint = process.env.PUBLIC_URL;
->>>>>>> cae007525626b767b9ac3a052008589fc8e3dc12
 
 //dirbant lokaliai per yarn start paleidus react app per localhost:3000
 //pakeisti localhost:8080 į vartus ant kurių paleidžiat aplikaciją:

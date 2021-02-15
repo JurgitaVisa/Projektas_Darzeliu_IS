8b96125a8133dd6a8e45cd4a0061149cda4ebe8e
Paulius <paujak@gmail.com>
2021-02-15 09:32:14 +0200

    pradzia darzelio ivedimo formos

--- front/src/components/05Kindengarten/KindergartenContainer.jsx
+++ front/src/components/05Kindengarten/KindergartenContainer.jsx
@@ -6,6 +6,7 @@ import '../../App.css';
 // import apiEndpoint from '../10Services/endpoint';
 
 import { KindergartenListContainer } from './KindergartenListContainer';
+import KindergartenInputForm from './KindergartenInputForm';
 
 export class KindergartenContainer extends Component {
 
@@ -18,6 +19,7 @@ export class KindergartenContainer extends Component {
                     <div className="row ">
                         <div className="bg-light pb-3 col-12 col-sm-12 col-md-12 col-lg-3">
                         <h6 className="py-3"><b>Pridėti naują darželį </b></h6>
+                            <KindergartenInputForm />
                         </div>
 
                         <div className="col-12 col-sm-12 col-md-12 col-lg-9 pt-1">
---
+++ front/src/components/05Kindengarten/KindergartenInputForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function KindergartenInputForm() {
+    return (
+        <div>
+            [bus naujo darželio įvedimo forma]
+        </div>
+    )
+}
+
+export default KindergartenInputForm

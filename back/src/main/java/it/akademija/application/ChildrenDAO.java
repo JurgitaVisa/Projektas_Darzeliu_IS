bbae896c19388c7de54550f22c6dda29ad288927
Daiva Liubiniene <daiva.silinskaite@gmail.com>
2021-02-18 10:32:41 +0200

    Prideta salt passw

--- back/src/main/java/it/akademija/application/ChildrenDAO.java
+++
@@ -1,9 +0,0 @@
-/*
- * package it.akademija.application;
- * 
- * import org.springframework.data.jpa.repository.JpaRepository;
- * 
- * public interface ChildrenDAO extends JpaRepository<Children, Long> {
- * 
- * }
- */

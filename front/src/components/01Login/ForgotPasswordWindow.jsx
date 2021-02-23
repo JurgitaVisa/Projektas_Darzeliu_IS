d03afacaa4cec95b5fa664efd395337e4c3c2b9e
gotouu21 <gotouu21@gmail.com>
2021-02-23 21:06:37 +0200

    Perdaryta forma su sweetalert2.1.2

--- front/src/components/01Login/ForgotPasswordWindow.jsx
+++ front/src/components/01Login/ForgotPasswordWindow.jsx
@@ -1,42 +1,28 @@
 import swal from "sweetalert";
 
-import React from 'react';
-import swalReact from '@sweetalert/with-react';
-
-import InputValidator from '../08CommonComponents/InputValidator';
-
-let formData = {
-    email: "test@test.com",
-}
-
 export default function ForgotPasswordWindow() {
-    console.log(formData)
-    return (
-        swalReact(
-            <div style={{padding: "34px"}}>
-                <h1>Slaptažodžio atstatymas</h1>
-                <form>
-                    <div className="form-group">
-                        <label htmlFor="email">Paskyros el. paštas <span className="fieldRequired">*</span></label>
-                    <input
-                        type="email"
-                        name="email"
-                        className="form-control"
-                        placeholder="Paskyros el. paštas"
-                        value={formData.email}
-                    />
-                    </div>
-                    <button
-                        type="submit"
-                        className="btn btn-primary float-right"
-                    >
-                        Atstatyti
-                    </button>
-                </form>
-            </div>,
-            {
-                buttons: false
-            }
-        )
-    )
+    swal({
+        title: "Slaptažodžio atstatymas",
+        text: "Įveskite paskyros el. paštą",
+        content: "input",
+        button: "Atstatyti",
+    }).then((userEmail) => {
+        const regexPattern = /\S+@\S+\.\S+/
+        if(regexPattern.test(userEmail)) {
+            swal({
+                title: "Slaptažodžio atstatymas",
+                text: "Staptažodžio atstatymo prašymas buvo sėkmingai išsiųstas administratoriui.",
+                button: "Baigti",
+                icon: "success",
+            })
+        }
+        else {
+            swal({
+                title: "Įvyko klaida",
+                text: "Neteisingas el. paštas",
+                button: "Gerai",
+                icon: "error"
+            })
+        }
+    })
 }

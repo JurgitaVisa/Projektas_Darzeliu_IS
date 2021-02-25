d2cbc9c406a064df5b8b425ca6463360619f5e73
gotouu21 <gotouu21@gmail.com>
2021-02-25 19:25:14 +0200

    uzdeta apsauga getAllRequests metodui

--- back/src/main/java/it/akademija/user/passwordresetrequests/UserPasswordResetRequestsController.java
+++ back/src/main/java/it/akademija/user/passwordresetrequests/UserPasswordResetRequestsController.java
@@ -8,19 +8,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import it.akademija.user.UserController;
 import it.akademija.user.UserService;
 
-@Controller
+@RestController
 @Api(value = "Password Reset Requests")
 @RequestMapping(path = "/passwordresetrequests")
 public class UserPasswordResetRequestsController {
@@ -31,6 +30,7 @@ public class UserPasswordResetRequestsController {
 	
 	private static final Logger LOG = LoggerFactory.getLogger(UserPasswordResetRequestsController.class);
 	
+	@Secured({ "ROLE_ADMIN" })
 	@GetMapping(path = "/getAllRequests")
 	@ApiOperation(value = "Returns a list of userId that want their password reset.")
 	public List<UserPasswordResetRequestsEntity> getAllPasswordResetRequests() {

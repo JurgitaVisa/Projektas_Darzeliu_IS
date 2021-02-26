abc9b532f5ba2a77986869a9d9968e25450c7b03
Daiva Liubiniene <daiva.silinskaite@gmail.com>
2021-02-26 11:07:17 +0200

    Isimtas JUnit updateUser metodas

--- back/src/test/java/it/akademija/user/UserRESTTest.java
+++ back/src/test/java/it/akademija/user/UserRESTTest.java
@@ -20,8 +20,6 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -101,37 +99,4 @@ public class UserRESTTest {
 
 	}
 
-	@Test
-	@WithMockUser(username = "manager", roles = { "MANAGER" })
-	public void testUpdateUserData() throws Exception {
-
-		/*
-		 * User updateUser = userService.findByUsername("test@test.lt");
-		 * updateUser.setName("Testas"); updateUser.setSurname("test");
-		 * updateUser.setEmail("test@test.lt");
-		 */
-
-		User updatedUser = new User(Role.MANAGER, "Testas", "Test", "test@test.lt", null, "test@test.lt",
-				"test@test.lt");
-
-		String jsonRequest = mapper.writeValueAsString(updatedUser);
-
-		/*
-		 * MvcResult postUpdated = mvc.perform( put("/api/users/update",
-		 * "test@test.lt").content(jsonRequest).contentType(MediaType.APPLICATION_JSON))
-		 * .andExpect(status().isOk()).andReturn(); assertEquals(200,
-		 * postUpdated.getResponse().getStatus());
-		 */
-
-		mvc.perform(MockMvcRequestBuilders.put("/api/users/update", "test@test.lt").content(jsonRequest)
-				.contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
-				.andExpect(MockMvcResultMatchers.jsonPath("$.role").value("MANAGER"))
-				.andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Testas"))
-				.andExpect(MockMvcResultMatchers.jsonPath("$.surname").value("Test"))
-				.andExpect(MockMvcResultMatchers.jsonPath("$.email").value("test@test.lt"))
-				.andExpect(MockMvcResultMatchers.jsonPath("$.parentDetails").value(null))
-				.andExpect(MockMvcResultMatchers.jsonPath("$.username").value("test@test.lt"))
-				.andExpect(MockMvcResultMatchers.jsonPath("$.password").value("test@test.lt"));
-
-	}
 }

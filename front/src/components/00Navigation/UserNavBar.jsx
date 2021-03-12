11157db66418d1669fd3d4b70f8d0caff0d10584
vg0t <vgot@mailbox.org>
2021-03-12 17:22:16 +0200

    navbar ikona, atidarymas naujame tabe

--- front/src/components/00Navigation/UserNavBar.jsx
+++ front/src/components/00Navigation/UserNavBar.jsx
@@ -4,6 +4,11 @@ import { NavLink } from 'react-router-dom';
 import logo from '../../images/logo.png';
 import '../../App.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons'
+
+import instructionsPdf from '../../documents/VDIS_naudotojo_gidas.pdf';
+
 import LogoutContainer from './LogoutContainer';
 
 function Navigation(props) {
@@ -22,24 +27,28 @@ function Navigation(props) {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav ml-auto ">
 
-                            <li className="nav-item mr-2">
+                            <li className="nav-item mr-1">
                                 <NavLink className="nav-link" id="navUserNewApplication" to={"/prasymai/naujas"}>Sukurti prašymą</NavLink>
                             </li>                        
 
-                            <li className="nav-item mr-2">
+                            <li className="nav-item mr-1">
                                 <NavLink className="nav-link" id="navUserMyApplications" to={"/prasymai"}>Mano prašymai</NavLink>
                             </li>
 
-                            <li className="nav-item mr-2">
+                            <li className="nav-item mr-1">
                                 <NavLink className="nav-link" id="navUserDocuments" to={"/pazymos"}>Mano pažymos</NavLink>
                             </li>
 
-                            <li className="nav-item mr-2">
+                            <li className="nav-item mr-1">
                                 <NavLink className="nav-link" id="navUserApplicationStats" to={"/statistika"}>Prašymų statistika</NavLink>
                             </li>
 
-                            <li className="nav-item mr-2">
+                            <li className="nav-item mr-1">
                                 <NavLink className="nav-link" id="navUserMyAccount" to={"/profilis/atnaujinti"}>Mano paskyra</NavLink>
+                            </li>
+
+                            <li className="nav-item mr-1">
+                                <a className="nav-link" id="navInstructions" href={instructionsPdf} target="_blank"><FontAwesomeIcon icon={faQuestionCircle} /></a>
                             </li>
 
                             <li className="nav-item nav-item mr-2">

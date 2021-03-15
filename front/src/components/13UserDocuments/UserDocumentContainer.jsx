2414f6c35b8c6ec33a5dff98955bfa29dd304358
vg0t <vgot@mailbox.org>
2021-03-15 17:01:50 +0200

    front: pazymos atsisiuntimas

--- front/src/components/13UserDocuments/UserDocumentContainer.jsx
+++ front/src/components/13UserDocuments/UserDocumentContainer.jsx
@@ -189,21 +189,25 @@ export default class UserDocumentContainer extends Component {
         })
     }
 
-    handleDownload = (document) => {
+    handleDownload = (doc) => {
         http.request({
-            url: `${apiEndpoint}/api/documents/get/${document.id}`,
+            url: `${apiEndpoint}/api/documents/get/${doc.id}`,
             method: "GET",
             responseType: "blob",
         }).then((response) => {
             const url = window.URL.createObjectURL(new Blob([response.data]));
             const link = document.createElement('a');
             link.href = url;
-            link.setAttribute('download', `${document.name}`);
+            link.setAttribute('download', `${doc.name}`);
             document.body.appendChild(link);
             link.click();
             link.remove();
         }).catch((error) => {
             console.log(error);
+            swal({
+                text: "Įvyko klaida atsisiunčiant pažymą.",
+                buttons: "Gerai",
+            })
         })
     }
 

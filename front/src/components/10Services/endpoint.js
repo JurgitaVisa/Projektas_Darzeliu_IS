23642a978d2be5c69eeb488207c897c46603f1ef
JurgitaVisa <jurgita.visakaviciute@gmail.com>
2021-03-17 19:32:49 +0200

    endpoint

--- front/src/components/10Services/endpoint.js
+++ front/src/components/10Services/endpoint.js
@@ -4,6 +4,4 @@ const apiEndpoint = process.env.PUBLIC_URL;
 //dirbant lokaliai per yarn start paleidus react app per localhost:3000
 //const apiEndpoint = "http://localhost:8080"
 
-//const apiEndpoint = "http://localhost:8089/darzelis"
-
 export default apiEndpoint;
